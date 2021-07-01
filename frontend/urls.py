eca0986823a9b91e293c941a8f23e62c7d574817
Billy Ashmall <billy.ashmall@nasa.gov>
2021-07-01 09:41:41 -0500

    Menu dynamic

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from . import views
 
-
+app_name = "ClimateSERV_UI"
 urlpatterns = [
     path('', views.index, name='home'),
     path('map', views.map_app, name='map'),
--- frontend/views.py
+++ frontend/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html', context={'nonya': 'business'})
+    return render(request, 'index.html', context={'page': 'menu-home'})
 
 
 def map_app(request):
-    return render(request, 'map.html', context={'nonya': 'business'})
+    return render(request, 'map.html', context={'page': 'menu-map'})
 
 
 def about(request):
-    return render(request, 'about.html', context={'nonya': 'business'})
+    return render(request, 'about.html', context={'page': 'menu-about'})
 
 
 def help_center(request):
-    return render(request, 'help.html', context={'nonya': 'business'})
+    return render(request, 'help.html', context={'page': 'menu-help'})
