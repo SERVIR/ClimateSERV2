598d83eb81c943bbd16083e57b6d1f9cc13969eb
Billy Ashmall <billy.ashmall@nasa.gov>
2021-07-13 15:22:14 -0500

    Mobile event debug

--- frontend/static/frontend/js/map.js
+++ frontend/static/frontend/js/map.js
@@ -651,3 +651,61 @@ function initMap() {
 $(function () {
   initMap();
 });
+
+(function( $ ) {
+
+  $.support.touch = typeof Touch === 'object';
+
+  if (!$.support.touch) {
+    return;
+  }
+
+  var proto =  $.ui.mouse.prototype,
+      _mouseInit = proto._mouseInit;
+
+  $.extend( proto, {
+    _mouseInit: function() {
+      this.element
+          .bind( "touchstart." + this.widgetName, $.proxy( this, "_touchStart" ) );
+      _mouseInit.apply( this, arguments );
+    },
+
+    _touchStart: function( event ) {
+      if ( event.originalEvent.targetTouches.length != 1 ) {
+        return false;
+      }
+
+      this.element
+          .bind( "touchmove." + this.widgetName, $.proxy( this, "_touchMove" ) )
+          .bind( "touchend." + this.widgetName, $.proxy( this, "_touchEnd" ) );
+
+      this._modifyEvent( event );
+
+      $( document ).trigger($.Event("mouseup")); //reset mouseHandled flag in ui.mouse
+      this._mouseDown( event );
+
+      return false;
+    },
+
+    _touchMove: function( event ) {
+      this._modifyEvent( event );
+      this._mouseMove( event );
+    },
+
+    _touchEnd: function( event ) {
+      this.element
+          .unbind( "touchmove." + this.widgetName )
+          .unbind( "touchend." + this.widgetName );
+      this._mouseUp( event );
+    },
+
+    _modifyEvent: function( event ) {
+      event.which = 1;
+      var target = event.originalEvent.targetTouches[0];
+      event.pageX = target.clientX;
+      event.pageY = target.clientY;
+    }
+
+  });
+
+})( jQuery );
