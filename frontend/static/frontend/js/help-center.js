f1e405136da0b81357489c5dfedd1afd2f3766e0
BillyZ313 <billy.ashmall@nasa.gov>
2021-08-24 16:35:12 -0500

    Help center - metadata

--- frontend/static/frontend/js/help-center.js
+++ frontend/static/frontend/js/help-center.js
@@ -38,26 +38,27 @@ let scroll_freeze = 0;
 
 function openDialog(metaData) {
     // close dialog if open to ensure proper load and scroll
+    const id = '"' + metaData.id + '"';
     if ($("#dialog").hasClass("ui-dialog-content") &&
         $("#dialog").dialog("isOpen")) {
         $('#dialog').dialog('close');
     }
     var html = '<div class="data-cards">';
     html +=
-        '<h1 style="width:100%; text-align: center;">' +
+        '<h1 style="width:100%; font-size: 120%; text-align: center; font-weight: 600; margin: 50px;">' +
         metaData.title +
         "</h1>";
     html += '<div class="col-md-6"> ';
     html += '<p class="abstract">' + metaData.abstract + "</p>";
-    html += "<h4>Credit:</h4>";
+    html += "<h4 style='font-size: 120%; font-weight: 600; margin-bottom: 25px;'>Credit:</h4>";
     html += "<p>" + metaData.credit + "</p>";
     html += "</div>";
     html += '<div class="col-md-6">';
     html +=
         '<img src="' +
         metaData.thumbnail +
-        '" style="max-width:100%; width:100%" onerror="imgError(this)">';
-    html += "</div>";
+        '" style="max-width:100%; width:100%" onerror="imgError(this)" class="meta-preview">';
+    html += "<button onclick='goto_full_record(" + id +")' class='meta-btn'>View full record</button></div>";
     html += "</div>";
 
     $("#dialog").html(html);
@@ -78,6 +79,10 @@ function openDialog(metaData) {
             document.documentElement.scrollTop = scroll_freeze;
         }
     });
+}
+
+function goto_full_record(which){
+    window.open("https://gis1.servirglobal.net/geonetwork/srv/eng/catalog.search#/metadata/" + which, "_meta_record");
 }
 
 function imgError(which){
@@ -167,6 +172,7 @@ function getMetaData(which) {
             }
 
             openDialog({
+                id: which,
                 title: title.replaceAll("\\\\n", "<br />"),
                 abstract: anchorme({
                     input: abstract
