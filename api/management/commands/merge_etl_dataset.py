21e21337f9a2fb2771493c3ac80ee1665b4e3e8c
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-10-22 11:14:10 -0500

    inverted year/month

--- api/management/commands/merge_etl_dataset.py
+++ api/management/commands/merge_etl_dataset.py
@@ -47,8 +47,8 @@ class Command(BaseCommand):
                 ncrcat_options = '-4 -h --cnk_dmn time,16 --cnk_dmn longitude,256 --cnk_dmn latitude,256'
             else:
                 pass
-            pattern_filepath = os.path.join(pattern_filepath, pattern_filename.format(MONTH_MM, YEAR_YYYY))
-            temp_aggregate_filepath = os.path.join(temp_aggregate_filepath, 'by_month/', aggregate_filename.format(MONTH_MM, YEAR_YYYY))
+            pattern_filepath = os.path.join(pattern_filepath, pattern_filename.format(YEAR_YYYY, MONTH_MM))
+            temp_aggregate_filepath = os.path.join(temp_aggregate_filepath, 'by_month/', aggregate_filename.format(YEAR_YYYY, MONTH_MM))
         else:
             if etl_dataset.dataset_subtype == 'chirps':
                 temp_fast_path = os.path.join('temp_fast_path', 'fast_chirps')
