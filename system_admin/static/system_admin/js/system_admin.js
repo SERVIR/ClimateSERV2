cd97a2b178516de0a3adb2ca83099bfafda2d1dd
BillyZ313 <billy.ashmall@nasa.gov>
2021-12-15 13:54:32 -0600

    Add FeatureCollection AOI to map

--- system_admin/static/system_admin/js/system_admin.js
+++ system_admin/static/system_admin/js/system_admin.js
@@ -1,4 +1,6 @@
 let bob;
+let map;
+let uploadLayer;
 
 function showAOI(which){
     let aoi;
@@ -6,6 +8,18 @@ function showAOI(which){
     bob = which;
     try{
         aoi = JSON.parse(which.innerText);
+
+        uploadLayer.clearLayers();
+        uploadLayer.addData(aoi);
+
+        try {
+                    map.fitBounds(uploadLayer.getBounds());
+                } catch(e) {
+            map.fitBounds([
+                [data.bbox[1], data.bbox[0]],
+                [data.bbox[3], data.bbox[2]],
+            ]);
+        }
     } catch(e){
         try {
 
@@ -19,3 +33,19 @@ function showAOI(which){
 function deleteRow(which){
     console.log("Delete: " + which);
 }
+
+function initMap() {
+    map = L.map("map", {
+        zoom: 3,
+        fullscreenControl: true,
+        center: [38.0, 15.0],
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    uploadLayer = L.geoJson().addTo(map);
+}
+
+$(function(){
+    initMap();
+});
