b70722716f99b7a78aa9eaf42211b593f2a7df35
BillyZ313 <billy.ashmall@nasa.gov>
2021-12-15 10:07:54 -0600

    items per page

--- system_admin/views.py
+++ system_admin/views.py
@@ -19,6 +19,7 @@ def usage(request):
     page = 1
     order_by = 'id'
     direction = "asc"
+    number_of_items = 2
     if request.method == "POST":
         if "delete_record_id" in request.POST:
             record = Track_Usage.objects.get(id=request.POST["delete_record_id"])
@@ -28,18 +29,25 @@ def usage(request):
             order_by = request.POST["sort_column"]
         if "direction" in request.POST:
             direction = 'desc' if request.POST["direction"] == 'asc' else 'asc'
+        if "number_of_items" in request.POST:
+            Track_Usage.objects.all().count()
+            post_count = request.POST["number_of_items"]
+            number_of_items = Track_Usage.objects.all().count() if post_count == "all" else post_count
+
     else:
         page = request.GET.get('page')
         if 'sorted' in request.GET:
             order_by = request.GET.get('sorted')
         if "direction" in request.GET:
             direction = request.GET.get('direction')
-    # order_by = "id"  # request.GET.get('order_by')
-    # direction = "asc"  # request.GET.get('direction')
+        if "number_of_items" in request.GET:
+            Track_Usage.objects.all().count()
+            get_count = request.GET.get("number_of_items")
+            number_of_items = Track_Usage.objects.all().count() if get_count == "all" else get_count
     ordering = order_by
     if direction == 'desc':
         ordering = '-{}'.format(ordering)
-    paginator = Paginator(Track_Usage.objects.all().order_by(ordering), 2)
+    paginator = Paginator(Track_Usage.objects.all().order_by(ordering), number_of_items)
 
     try:
         items = paginator.page(page)
@@ -53,7 +61,6 @@ def usage(request):
     start_index = index - 5 if index >= 5 else 0
     end_index = index + 5 if index <= max_index - 5 else max_index
     page_range = paginator.page_range[start_index:end_index]
-
     context = {
         'headers': Track_Usage._meta.get_fields(),
         'usage': Track_Usage.objects.all(),
@@ -61,7 +68,8 @@ def usage(request):
         'items': items,
         'page': page,
         'sorted': order_by,
-        'direction': direction
+        'direction': direction,
+        'number_of_items': number_of_items,
     }
     return render(request, 'usage.html', context)
 
