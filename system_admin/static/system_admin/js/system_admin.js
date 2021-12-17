7cd7c36d2d3fe22c7918add93d10ef39baf4bea1
BillyZ313 <billy.ashmall@nasa.gov>
2021-12-17 12:00:02 -0600

    Display Admin feature AOIs

--- system_admin/static/system_admin/js/system_admin.js
+++ system_admin/static/system_admin/js/system_admin.js
@@ -1,24 +1,47 @@
 let bob;
 let map;
 let uploadLayer;
+let adminHighlightLayer;
+const admin_layer_url = location.hostname === "localhost" ||
+location.hostname === "127.0.0.1" ||
+location.hostname === "192.168.1.132"
+    ? "https://climateserv2.servirglobal.net/servirmap_102100/?&crs=EPSG%3A102100"
+    : "servirmap_102100/?&crs=EPSG%3A102100";
 
 function showAOI(which) {
     let aoi;
 
     bob = which;
     try {
-        aoi = JSON.parse(which.innerText);
-
         uploadLayer.clearLayers();
-        uploadLayer.addData(aoi);
-
-        try {
-            map.fitBounds(uploadLayer.getBounds());
-        } catch (e) {
-            map.fitBounds([
-                [data.bbox[1], data.bbox[0]],
-                [data.bbox[3], data.bbox[2]],
-            ]);
+        if (adminHighlightLayer) {
+            adminHighlightLayer.remove();
+        }
+        aoi = JSON.parse(which.innerText);
+        if(aoi["Admin Boundary"]){
+            adminHighlightLayer = L.tileLayer.wms(
+                            admin_layer_url,
+                            {
+                                layers: aoi["Admin Boundary"] + "_highlight",
+                                format: "image/png",
+                                transparent: true,
+                                styles: "",
+                                TILED: true,
+                                VERSION: "1.3.0",
+                                feat_ids: aoi["FeatureIds"].join(),
+                            }
+                        );
+            map.addLayer(adminHighlightLayer);
+        } else {
+            uploadLayer.addData(aoi);
+            try {
+                map.fitBounds(uploadLayer.getBounds());
+            } catch (e) {
+                map.fitBounds([
+                    [data.bbox[1], data.bbox[0]],
+                    [data.bbox[3], data.bbox[2]],
+                ]);
+            }
         }
     } catch (e) {
         try {
