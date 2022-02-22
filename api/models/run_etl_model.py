89d8712e35de94798184b38065667fa2fa195b0d
BillyZ313 <billy.ashmall@nasa.gov>
2022-02-22 09:25:07 -0600

    run_etl_foreignKey

--- api/models/run_etl_model.py
+++ api/models/run_etl_model.py
@@ -2,20 +2,20 @@ from django.db import models
 
 from api.models import ETL_Dataset
 
+
 class Run_ETL(models.Model):
-    datasets = ETL_Dataset.objects.all()
-    DS=()
-    for x in range(len(datasets)):
-        DS=DS+((datasets[x].uuid,datasets[x].dataset_name,),)
-    etl = models.CharField(max_length=60, choices=DS, default='CHIRPS')
+    etl = models.ForeignKey(ETL_Dataset,
+                            on_delete=models.SET_NULL,
+                            blank=True,
+                            null=True)
     start_year = models.CharField(max_length=4, blank=False, default="2020")
-    end_year = models.CharField(max_length=4, blank=False,default="2020")
-    start_month = models.CharField(max_length=2, blank=False,default="1")
-    end_month = models.CharField(max_length=2, blank=False,default="12")
-    start_day = models.CharField(max_length=2, blank=False,default="1")
-    end_day = models.CharField(max_length=2, blank=False,default="30")
+    end_year = models.CharField(max_length=4, blank=False, default="2020")
+    start_month = models.CharField(max_length=2, blank=False, default="1")
+    end_month = models.CharField(max_length=2, blank=False, default="12")
+    start_day = models.CharField(max_length=2, blank=False, default="1")
+    end_day = models.CharField(max_length=2, blank=False, default="30")
     from_last_processed = models.BooleanField(default=False)
-    merge_periodically = models.BooleanField("Merge",default=False)
+    merge_periodically = models.BooleanField("Merge", default=False)
 
     class Meta:
         verbose_name = 'ETL Run'
