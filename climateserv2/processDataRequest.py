76131fd6a80afe5e6a82c01573028ae93d4853d6
BillyZ313 <billy.ashmall@nasa.gov>
2022-07-11 13:36:26 -0500

    debug

--- climateserv2/processDataRequest.py
+++ climateserv2/processDataRequest.py
@@ -435,18 +435,18 @@ def start_worker_process(job_item):
                 # This is the line that randomly hangs and will not recover
                 # t_lock = threading.Lock()
                 # t_lock.acquire()
-                db.connection.ensure_connection()
-                request_progress = Request_Progress.objects.get(request_id=uniqueid)
-                logger.debug("got request object for: " + uniqueid)
-                logger.info(str(job_length) + ' - was the job_length')
-                update_value = (float(request_progress.progress) + (100 / job_length)) - .5
-                logger.info(str(update_value) + '% done')
-                # this is so the progress is not set to 100 before the output files are saved to the drive
-                # once saved it will update to 100.
-                request_progress.progress = update_value
-                logger.debug("updated progress for: " + uniqueid)
-                request_progress.save()
-                logger.debug(str(job_item["uniqueid"]) + "***************************** " + str(request_progress.progress))
+                # db.connection.ensure_connection()
+                # request_progress = Request_Progress.objects.get(request_id=uniqueid)
+                # logger.debug("got request object for: " + uniqueid)
+                # logger.info(str(job_length) + ' - was the job_length')
+                # update_value = (float(request_progress.progress) + (100 / job_length)) - .5
+                # logger.info(str(update_value) + '% done')
+                # # this is so the progress is not set to 100 before the output files are saved to the drive
+                # # once saved it will update to 100.
+                # request_progress.progress = update_value
+                # logger.debug("updated progress for: " + uniqueid)
+                # request_progress.save()
+                # logger.debug(str(job_item["uniqueid"]) + "***************************** " + str(request_progress.progress))
             except:
                 logger.error("error getting or updating progress")
                 pass
