467d0f1d8ba801868a658ea74c77e7b70e1e36ac
BillyZ313 <billy.ashmall@nasa.gov>
2023-10-06 10:19:06 -0500

    csv, xls date fix

--- frontend/static/frontend/js/map.js
+++ frontend/static/frontend/js/map.js
@@ -2773,7 +2773,8 @@ function multi_chart_builder(conversion) {
 
                 // const adjustedDate = new Date(date.getTime() + offset * 60000);
                 // const date_string = adjustedDate.getUTCMonth() + 1 + "/" + adjustedDate.getDate() + "/" + adjustedDate.getUTCFullYear();
-                const date_string = date.getUTCMonth() + 1 + "/" + date.getUTCDate() + "/" + date.getUTCFullYear();
+                // const date_string = date.getUTCMonth() + 1 + "/" + date.getUTCDate() + "/" + date.getUTCFullYear();
+                const date_string = date.getUTCFullYear() + "-" + date.getUTCDate() + "-" + date.getUTCMonth() + " 00:00:00";
                 console.log(date_string);
                 csvContent += `${date_string},`;
                 multiQueryData.forEach((item) => {
@@ -2844,7 +2845,8 @@ function multi_chart_builder(conversion) {
 
                 // const adjustedDate = new Date(date.getTime() + offset * 60000);
                 // const date_string = adjustedDate.getUTCMonth() + 1 + "/" + adjustedDate.getDate() + "/" + adjustedDate.getUTCFullYear();
-                const date_string = date.getUTCMonth() + 1 + "/" + date.getUTCDate() + "/" + date.getUTCFullYear();
+                // const date_string = date.getUTCMonth() + 1 + "/" + date.getUTCDate() + "/" + date.getUTCFullYear();
+                const date_string = date.getUTCFullYear() + "-" + date.getUTCDate() + "-" + date.getUTCMonth() + " 00:00:00";
 
                 const row = [date_string];
 
